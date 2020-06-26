e9dfe1c7381793325681a9dbd89ab8c6e78b0a05
user <user@192.168.0.10>
2020-06-25 21:16:05 -0400

    Welcome Program

---
+++ Practice/src/hari/practice/corejava/Hellowelcome.java
@@ -0,0 +1,13 @@
+package hari.practice.corejava;
+
+public class Hellowelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi, Welcome. Practice gives more knowledge. Always Do your best!!");
+		System.out.println("Best of luck for your Practice");
+
+	}
+
+}
